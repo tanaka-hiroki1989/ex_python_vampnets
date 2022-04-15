f6b1810735e851493613b209ff176f46321bd657
Hiroki <rokitaniplus1@gmail.com>
2022-04-15 16:09:31 +0900

    dihedral

--- ala2/main.py
+++ ala2/main.py
@@ -13,3 +13,18 @@ else:
 torch.set_num_threads(12)
 
 print(f"Using device {device}")
+
+with np.load("data/alanine-dipeptide-3x250ns-heavy-atom-positions.npz") as fh:
+    data = [fh[f"arr_{i}"].astype(np.float32) for i in range(3)]
+with np.load("data/alanine-dipeptide-3x250ns-backbone-dihedrals.npz") as fh:
+    dihedral = [fh[f"arr_{i}"] for i in range(3)]
+
+def plot_dihedral(dihedral,image_path):
+    f, ax = plt.subplots(1, 1)
+    hb = ax.hexbin(*np.concatenate(dihedral).T, mincnt=5)
+    ax.set_aspect('equal')
+    cb = f.colorbar(hb, ax=ax)
+    cb.set_label('# of frames in bin')
+    plt.savefig(image_path)
+
+plot_dihedral(dihedral,"fig/dihedral_plane.png")
